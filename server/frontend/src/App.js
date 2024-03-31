1b8c271301b8d8cccf3a8b7bfee9fab52d91ecf7
Thiago Mena <menathiagx@gmail.com>
2024-03-31 18:27:28 -0300

    idk what happen

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,12 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
 
 function App() {
   return (
     <Routes>
+      <Route path="/dealers" element={<Dealers/>} />
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
     </Routes>
